bf55fe6eb38d4c9dc6e0be49ea0c241af30b30b0
SilverXuz <silver-xuz@mail.ru>
2022-07-17 20:15:58 +0300

    2 Программа, которая ищет второй максимум.

---
+++ SM/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите число от 0 до 100: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int secondMax = 0;
+while(n != 0)
+    {
+        Console.Write("Введите число от 0 до 100: ");
+        n = Convert.ToInt32(Console.ReadLine());
+        if (n > max)
+        {
+            secondMax = max;
+            max = n;
+        }
+        else
+            if (n > secondMax)
+                secondMax = n;
+    }
+Console.WriteLine("Второе максимальное число -> " + secondMax);
